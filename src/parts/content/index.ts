b2aefc551dd94d0e09a2e771a7518df3456fa42f
akzhy <akshay48884@gmail.com>
2021-03-30 12:11:11 +0530

    :pencil: Add some code comments

--- src/parts/content/index.ts
+++ src/parts/content/index.ts
@@ -5,8 +5,12 @@ type ResponseObj = Record<
     }
 >;
 
+// Listen for message from popup
+// Message is sent when a user opens the popup by clicking the icon.
+
 chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
     if (request.action === 'get-meta-values') {
+        // Fetch all meta tags
         const metaElements = document.head.querySelectorAll('meta');
         const obj: ResponseObj = {
             og: {},
@@ -15,6 +19,7 @@ chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
                 title: document.title,
             },
         };
+
         metaElements.forEach((node) => {
             const property = node.getAttribute('property');
             const name = node.getAttribute('name');
